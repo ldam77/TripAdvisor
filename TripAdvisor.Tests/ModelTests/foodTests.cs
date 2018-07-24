df389f5371ce0d5522db463fcace8f8b811f95f5
Jean Jia <Jean84646@gmail.com>
2018-07-24 11:00:21 -0700

    Add files.

--- TripAdvisor.Tests/ModelTests/foodTests.cs
+++ TripAdvisor.Tests/ModelTests/foodTests.cs
@@ -16,7 +16,7 @@ namespace TripAdvisor.Tests
     }
     public foodTests()
     {
-      DBConfiguration.ConnectionString = "server=localhost;user id=root;password=root;port=8889;database=trip_advisor_test;";
+      DBConfiguration.ConnectionString = "server=localhost;user id=root;password=root;port=3306;database=trip_advisor_test;";
     }
     // Get Name
     [TestMethod]
