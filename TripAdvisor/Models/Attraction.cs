a9abaa4b2538b235aaf323a4a0e0d428045b45fd
Lan Dam <ldam77@yahoo.com>
2018-07-26 10:32:04 -0700

    add delete to attraction.cs and tests

--- TripAdvisor/Models/Attraction.cs
+++ TripAdvisor/Models/Attraction.cs
@@ -191,6 +191,24 @@ namespace TripAdvisor.Models
       }
     }
 
+    public void Delete()
+    {
+      MySqlConnection conn = DB.Connection();
+      conn.Open();
+      MySqlCommand cmd = conn.CreateCommand() as MySqlCommand;
+      cmd.CommandText = @"DELETE FROM attractions WHERE id = @Id;";
+      MySqlParameter searchId = new MySqlParameter();
+      searchId.ParameterName = "@Id";
+      searchId.Value = this.id;
+      cmd.Parameters.Add(searchId);
+      cmd.ExecuteNonQuery();
+      conn.Close();
+      if (conn != null)
+      {
+        conn.Dispose();
+      }
+    }
+
     public static void DeleteAll()
     {
       MySqlConnection conn = DB.Connection();
